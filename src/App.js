508238771290c3dbef4478f687f3cc5b72821eae
khushboo kanojiya <khushboo.kanojiya099@.com>
2024-11-17 14:12:25 +0530

    understood react componet structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import UserProfile from './components/userProfile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <UserProfile />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -14,10 +16,11 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/components/userProfile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './userProfile.css'; // We'll create this CSS file next
+
+const UserProfile = () => {
+    return (
+        <div className="user-profile">
+            <h1>User Profile</h1>
+            <p>This is your social media user profile page!</p>
+            {/* Add more HTML-like JSX here */}
+        </div>
+    );
+};
+
+export default UserProfile;
