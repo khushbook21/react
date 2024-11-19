a5143f13a2347472d37c0145ee2a2fb71ccbf42a
khushboo kanojiya <khushboo@marketsmojo.com>
2024-11-19 10:39:02 +0530

    changes done

--- src/components/userProfile.js
+++ src/components/userProfile.js
@@ -5,7 +5,7 @@ const UserProfile = () => {
     return (
         <div className="user-profile">
             <h1>User Profile</h1>
-            <p>This is your social media user profile page!</p>
+            <p>This is your social media user profile page! check it now</p>
             {/* Add more HTML-like JSX here */}
         </div>
     );
