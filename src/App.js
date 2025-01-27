ac92798b1ad1a5f2fcc0c03310113ce3570d7e68
khushboo kanojiya <khushboo@marketsmojo.com>
2025-01-27 11:42:34 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-// import logo from './logo.svg';
 import './App.css';
 import UserProfile from './components/userProfile';
-import ReactLifecycle from './components/ReactLifecycle';
+
 
 function App() {
   return (
     <div className="App">
       <UserProfile />
-      <ReactLifecycle />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          
-        >
-          Learn React
-        </a>
-      </header> */}
+
     </div>
   );
 }
--- src/components/userProfile.js
+++ src/components/userProfile.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './userProfile.css'; // We'll create this CSS file next
+import './userProfile.css';
 
 const UserProfile = () => {
     return (
         <div className="user-profile">
             <h1>User Profile</h1>
             <p>This is your social media user profile page!</p>
-            {/* Add more HTML-like JSX here */}
+
         </div>
     );
 };
