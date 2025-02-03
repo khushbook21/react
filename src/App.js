2c3c4711fc88102606a71a8470f8e680464a540a
khushboo kanojiya <khushboo@marketsmojo.com>
2025-02-03 15:08:39 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-// import UserProfile from './components/userProfile';
-// import UserCards from './components/UserCards';
-
 import LectureOne from './components/LectureOne';
-
-
 function App() {
   return (
     <div className="App">
-      {/* <UserProfile /> */}
-      {/* <UserCards /> */}
       <LectureOne />
-
     </div>
   );
 }
