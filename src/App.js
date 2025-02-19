cd4fb3c6a929494e2e0240ff7f567bf9360050e0
khushboo kanojiya <khushboo@marketsmojo.com>
2025-02-19 15:11:15 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -48,13 +48,16 @@ function App() {
   function handleInputChange() {
     console.log('on input change the count');
   }
+
   return (
 
     <div>
       <form>
         <input type="text" onChange={handleInputChange} />
- 
+
       </form>
+      <img className="avtar" src='https://i.imgur.com/1bX5QH6.jpg' alt="this is image " width={100} height={100} style={{padding:"10px"}}/>
+
       {/* <p onMouseOver={handleMouseOver} style={{ color: "red", padding:"10px" }}>Im a para </p>
       <button onClick={hnadleClick}>click me</button> */}
     </div>
